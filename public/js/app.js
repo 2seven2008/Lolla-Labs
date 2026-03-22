f7555cb4ff14946b6cc1df9744829a89bac11d1f
2seven2008 <orlsgabriel09@gmail.com>
2026-03-21 22:12:57 -0300

    Fix Issues in Doc

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/public/sw.js")
+      .register("./sw.js")
       .then((reg) => console.log("[SW] Registered:", reg.scope))
       .catch((err) => console.warn("[SW] Registration failed:", err));
   });
